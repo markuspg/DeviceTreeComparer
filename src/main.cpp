3201490f7eeca092513071d142d43d0c8c00b916
markuspg <markuspg@users.noreply.github.com>
2020-03-30 22:33:31 +0200

    Add very basic commandline interface

--- src/main.cpp
+++ src/main.cpp
@@ -22,30 +22,54 @@
  */
 
 #include "device_tree_parser.h"
+#include "root_node.h"
 
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    std::cerr << "Exactly two arguments are required - the two files to be "
-                 "compared\n";
-    return 1;
+  if (argc < 2) {
+    std::cerr << "At least two arguments are required - the two files to be "
+                 "compared (a help text can be accessed with \"-h\")\n";
+    return 2;
   }
 
-  const std::string file1{argv[1]};
-  const std::string file2{argv[2]};
+  bool displayHelp = false;
+  for (auto i = 1; i < argc; ++i) {
+    if (std::string{argv[i]} == "-h") {
+      displayHelp = true;
+    }
+  }
+
+  if (displayHelp) {
+    std::cout << "DeviceTreeComparer [OPTIONS] FILE_1 FILE_2\n\n"
+              << "Without any options this tool compares the two device tree "
+                 "source files and\nreturns '0' if they are equal or '1' if "
+                 "they differ.\n\n"
+              << "Options:\n"
+              << "\t-h: Display this help text\n";
+    return 0;
+  }
+
+  if (argc < 3) {
+    std::cerr << "At least two positional arguments are required - the two "
+                 "files to be compared\n";
+    return 3;
+  }
+
+  const std::string file1{argv[argc - 2]};
+  const std::string file2{argv[argc - 1]};
 
   DeviceTreeParser parserFile1{file1};
   const auto rootNode1 = parserFile1.ParseFile();
   if (!rootNode1) {
     std::cerr << "Failed to parse file: " << file1 << "\n";
-    return 2;
+    return 4;
   }
   DeviceTreeParser parserFile2{file2};
   const auto rootNode2 = parserFile2.ParseFile();
   if (!rootNode2) {
     std::cerr << "Failed to parse file: " << file2 << "\n";
-    return 3;
+    return 5;
   }
   CompareNodes(*rootNode1.get(), *rootNode2.get());
 
